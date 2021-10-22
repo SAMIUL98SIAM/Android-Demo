0cd547b1d677544a50066ba3d3a0dff71d1d1455
SAMIUL98SIAM <samiulsiam89@gmail.com>
2021-10-22 20:44:56 +0600

    TextView Demo

---
+++ MyTextViewDemo/app/src/main/java/com/example/mytextviewdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mytextviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTextView = (TextView) findViewById(R.id.nameTextViewId);
+        nameTextView.setText("Dr.Samiul");
+    }
+}
